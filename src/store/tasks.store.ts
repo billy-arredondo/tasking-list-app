99df4f4d41df524f8c5d07ba97c48394364601fb
Billy Arredondo <billyarredondo.coder@gmail.com>
2026-04-22 15:42:46 +0200

    Refactor tasks store to use Zustand middleware for persistence

--- src/store/tasks.store.ts
+++ src/store/tasks.store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { getNewId } from "../helpers/task.helper";
 import { descriptionType, idType, Task } from "../models/Tasks.model";
 
@@ -10,34 +11,39 @@ type TasksStore = {
   reorderTasks: (fromIndex: number, toIndex: number) => void;
 };
 
-export const useTasksStore = create<TasksStore>((set) => ({
-  tasks: [],
-  addTask: (description) =>
-    set((state) => ({
-      tasks: [
-        ...state.tasks,
-        {
-          id: getNewId(state.tasks),
-          description,
-          done: false,
-        },
-      ],
-    })),
-  toogleTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.map((task) =>
-        task.id === id ? { ...task, done: !task.done } : task
-      ),
-    })),
-  removeTask: (id) =>
-    set((state) => ({
-      tasks: state.tasks.filter((task) => task.id !== id),
-    })),
-  reorderTasks: (fromIndex, toIndex) =>
-    set((state) => {
-      const updated = [...state.tasks];
-      const [moved] = updated.splice(fromIndex, 1);
-      updated.splice(toIndex, 0, moved);
-      return { tasks: updated };
+export const useTasksStore = create<TasksStore>()(
+  persist(
+    (set) => ({
+      tasks: [],
+      addTask: (description) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            {
+              id: getNewId(state.tasks),
+              description,
+              done: false,
+            },
+          ],
+        })),
+      toogleTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id ? { ...task, done: !task.done } : task
+          ),
+        })),
+      removeTask: (id) =>
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+        })),
+      reorderTasks: (fromIndex, toIndex) =>
+        set((state) => {
+          const updated = [...state.tasks];
+          const [moved] = updated.splice(fromIndex, 1);
+          updated.splice(toIndex, 0, moved);
+          return { tasks: updated };
+        }),
     }),
-}));
+    { name: "tasks" }
+  )
+);
